334dcbbc1a8fb25f4dce923438cc5f354d19d5fb
TUNCERAKALIN <tuncerakalin407@gmail.com>
2019-12-24 09:13:44 -0800

    memegenerator

---
+++ memegenerator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MemeGenerator from "./Components/MemeGenerator"
+import './App.css';
+import Header from "./Components/Header"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <MemeGenerator/>
+    </div>
+  );
+}
+
+export default App;
